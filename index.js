479d0c485fc11b22dc9cae4f1cb7a5490705b58c
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-02-21 21:59:56 +0000

    set of questions added

--- index.js
+++ index.js
@@ -5,8 +5,61 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+      type: 'input',
+      message: 'Title',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Project Description',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: "Installation",
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: "Usage?",
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      choices: ['Apache', 'MIT', 'GLP'],
+      message: "License",
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: "Contribution",
+      name: 'contribution',
+    },
+    {
+      type: 'input',
+      message: "Test",
+      name: 'test',
+    },
+    {
+      type: 'input',
+      message: "Questions",
+      name: 'questions',
+    },
+    {
+      type: 'input',
+      message: "e-mail",
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: "GitHub",
+      name: 'github',
+    },
+  ];
 
-];
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
