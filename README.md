09d21c72c219ce5cb1f5f2f606929758c278eed1
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-02-21 22:26:31 +0000

    added markdown generator

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# readme-generator
+# titulo
+      [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+      # aff
+      
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,26 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(response) {
+  let readmeContent = ""
+    if (response.title) {
+      readmeContent += `# ${response.title}
+      [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+      `
+    };
+    if (response.description) {
+      readmeContent += `# ${response.description}
+      `
+    };
 
-`;
+
+
+
+    
+
+
+
+    
+  
+  return readmeContent
 }
 
 module.exports = generateMarkdown;
