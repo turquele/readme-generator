f327f2af99f1045e38eeecd858efa7cc1ac697dd
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-02-21 22:02:43 +0000

    added license types and function to write file

--- index.js
+++ index.js
@@ -58,11 +58,19 @@ const questions = [
     },
   ];
 
-
+// License types
+const icenses = [
+    {'Apache' : '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'},
+    {'MIT' : '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'},
+    {'GNU' : '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'},
+  ];
 
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile('README.md', data, (err) =>
+    err ? console.error(err) : console.log('Success!')
+    );
 }
 
 // function to initialize program
